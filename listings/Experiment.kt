f0d0a0e6a429dd7a00604c86f71c7d60751ad0a1
jjurm <micko.juraj64@gmail.com>
2020-04-19 17:13:46 +0400

    Improve code listing

--- listings/Experiment.kt
+++ listings/Experiment.kt
@@ -1,5 +1,7 @@
 package uk.ac.cam.jm2186.graffs.db.model
 
+import org.hibernate.annotations.LazyCollection
+import org.hibernate.annotations.LazyCollectionOption
 import uk.ac.cam.jm2186.graffs.db.NamedEntity
 import uk.ac.cam.jm2186.graffs.graph.storage.GraphDatasetId
 import uk.ac.cam.jm2186.graffs.metric.MetricId
@@ -8,21 +10,24 @@ import javax.persistence.*
 
 @Entity
 class Experiment(
-    name: String,
+        name: String,
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    var generator: GraphGenerator,
-    @ElementCollection(fetch = FetchType.EAGER)
-    var metrics: Set<MetricId> = mutableSetOf(),
-    @ElementCollection(fetch = FetchType.EAGER)
-    var robustnessMeasures: Set<RobustnessMeasureId> = mutableSetOf(),
+        @ManyToOne(fetch = FetchType.EAGER)
+        var generator: GraphGenerator,
+        @ElementCollection(fetch = FetchType.EAGER)
+        var metrics: Set<MetricId> = mutableSetOf(),
+        @ElementCollection(fetch = FetchType.EAGER)
+        var robustnessMeasures: Set<RobustnessMeasureId> = mutableSetOf(),
 
-    datasets: Collection<GraphDatasetId> = listOf()
+        datasets: Collection<GraphDatasetId> = listOf()
 ) : NamedEntity(name) {
 
-    @OneToMany(mappedBy = "experiment", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.EAGER)
-    var graphCollections: MutableList<GraphCollection> = datasets.map { GraphCollection(it) }.toMutableList()
-    @OneToMany(mappedBy = "experiment", cascade = [CascadeType.REMOVE], orphanRemoval = true, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "experiment", cascade = [CascadeType.ALL], orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    var graphCollections: MutableList<GraphCollection> = datasets.map { GraphCollection(it, this) }.toMutableList()
+
+    @OneToMany(mappedBy = "experiment", cascade = [CascadeType.REMOVE], orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
     val robustnessResults: MutableList<Robustness> = mutableListOf()
 
     val datasets get() = graphCollections.map { it.dataset }
