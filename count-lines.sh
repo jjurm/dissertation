31a6ebba25a09b937b2530a3fcad47a485b41c4a
jjurm <micko.juraj64@gmail.com>
2020-05-18 16:50:23 +0200

    Light content improvements

--- count-lines.sh
+++ count-lines.sh
@@ -1,1 +1,2 @@
-cloc --include-lang="Kotlin,Gradle,Python,CSS,YAML,SQL,Bourne Again Shell" --exclude-d=.idea --quiet --hide-rate --csv graffs partii | tail -n 1 | awk -F, '{print $3+$4+$5}'
+cloc --include-lang="Kotlin,Gradle,Python,CSS,YAML,SQL,Bourne Again Shell" --exclude-d=.idea \
+  --quiet --hide-rate --csv graffs partii | tail -n 1 | awk -F, '{print $3+$4+$5}'
